bee22889906f49d2450504dac9f00b6f3ea0fcf3
Humoyunmirzo <humoyunmirzoxoliqberdiyev737@gmail.com>
2023-08-10 14:24:21 +0500

    commit 1

--- pages/api/teams.js
+++ pages/api/teams.js
@@ -1,6 +1,6 @@
-import { cors, runMiddleware } from "@/backend-functions/middleware";
+// import { cors, runMiddleware } from "@/backend-functions/middleware";
 import { teams } from "@/data/teams";
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors)
+export default  function handler(req, res) {
+  // await runMiddleware(req, res, cors)
   res.status(200).json(teams)
 }
--- pages/api/teams/[team].js
+++ pages/api/teams/[team].js
@@ -1,7 +1,7 @@
-import { cors, runMiddleware } from "@/backend-functions/middleware";
+// import { cors, runMiddleware } from "@/backend-functions/middleware";
 import { teams } from "@/data/teams";
-export default async function handler(req, res) {
-    await runMiddleware(req, res, cors)
+export default  function handler(req, res) {
+    // await runMiddleware(req, res, cors)
     const teamName = req.query.team
     const team = teams.find(item => item.slug == teamName)
     res.status(200).json(team)
