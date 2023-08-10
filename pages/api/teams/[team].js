57ea596acb489c68da6e9ff7dd24ba94c6bd7978
Humoyunmirzo <humoyunmirzoxoliqberdiyev737@gmail.com>
2023-08-10 10:18:36 +0500

    init

--- pages/api/teams/[team].js
+++ pages/api/teams/[team].js
@@ -1,4 +1,8 @@
 import { teams } from "@/data/teams";
+import Cors from 'cors'
+const cors = Cors({
+    methods:["GET","HEAD"]
+})
 export default function handler(req, res) {
     const teamName = req.query.team
     const team = teams.find(item => item.slug == teamName)
