6dfc62d65940468785c69ca53854df6d8e3eafba
Humoyunmirzo <humoyunmirzoxoliqberdiyev737@gmail.com>
2023-08-10 18:08:40 +0500

    deleted files

--- pages/api/teams/[team].js
+++ pages/api/teams/[team].js
@@ -1,6 +1,6 @@
 // import { cors, runMiddleware } from "@/backend-functions/middleware";
 import { teams } from "@/data/teams";
-export default  function handler(req, res) {
+export default function handler(req, res) {
     // await runMiddleware(req, res, cors)
     const teamName = req.query.team
     const team = teams.find(item => item.slug == teamName)
