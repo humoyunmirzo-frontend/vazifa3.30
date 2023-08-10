ca976dd4a2004509796a26a98ae0ffb4a443878a
Humoyunmirzo <humoyunmirzoxoliqberdiyev737@gmail.com>
2023-08-10 17:47:31 +0500

    deploying time

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api/"
+    baseURL: "https://vazifa3-30.vercel.app/api/"
 })
 const getTeamsStrength = async () => {
     try {
